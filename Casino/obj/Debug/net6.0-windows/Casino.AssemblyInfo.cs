ce5bd6540a71d9a746be0aa87a810c3a8dc9a2e9
RASAN\psameh <psameh@rasan.co>
2024-04-29 12:42:44 +0300

    add staged changes

--- Casino/obj/Debug/net6.0-windows/Casino.AssemblyInfo.cs
+++ Casino/obj/Debug/net6.0-windows/Casino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Casino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f887614624ff11a1bd1c9d77c2148581c81be33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Casino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Casino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
